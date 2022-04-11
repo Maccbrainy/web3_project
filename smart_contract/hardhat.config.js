6dc5a894953cb878abf63e2b0a812729dca05008
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-04-11 17:05:07 +0100

    Hardhat environmental variable setup

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const { ALCHEMY_HTTP_ROPSTEN_API_KEY, METAMASK_PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url:`${ALCHEMY_HTTP_ROPSTEN_API_KEY}`,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+    }
+  }
 };
