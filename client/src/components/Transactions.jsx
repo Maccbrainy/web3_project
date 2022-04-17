5e442995b47196ad7f5dcb3418b8567ed549c0fc
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-04-17 15:19:50 +0100

    Transactions update when account is connected to metamask

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import {TransactionContext}  from "../context/transactionContext";
+
 const Transactions = () => {
+    const {currentAccount} = useContext(TransactionContext);
     return (
-        <h1>Transactions</h1>
+        <div className="gradient-bg-transactions">
+           { currentAccount ? (<h1 className="text-white text-center font-normal text-2xl">Latest Transactions</h1>) : (<h1 className="text-white font-normal text-2xl">Connect your account to see the latest transactions</h1>)}
+                      
+        </div>
     )
 }
 export default Transactions;
