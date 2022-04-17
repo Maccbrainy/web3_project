b5348610789a25f17704f8ddee52cad207c28736
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-04-17 16:16:30 +0100

    Initial commit

---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,65 @@
+export default [
+    {
+        id: 1,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+    {
+        id: 2,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+    {
+        id: 3,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+    {
+        id: 4,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+    {
+        id: 5,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+    {
+        id: 6,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+    {
+        id: 7,
+        timestamp: "04/17/2022, 3:40:33pm",
+        message:"",
+        addressFrom:"0xhdknkmnjndkjn33njkbr3k298032",
+        addressTo:"bd48j89ep23me3k2mdjkdoidji837h3e8",
+        amount:"0.01",
+        url:""
+    },
+]
