435a815eb210e8ed7df4cbdeb8ead3c71207f632
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-04-19 08:32:29 +0100

    Modified the loader and added props features for more usability

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,6 +1,9 @@
-const Loader = () => {
-    return (
-        <h1>Loader</h1>
+const Loader = ({title, height, width}) => {
+     return (
+        <div className="flex justify-center items-center w-full mt-2 p-2">
+            <div className={`animate-spin rounded-full ${height} ${width} border-b-2 border-white`}/>
+            <span className="ml-4 text-white">{title}</span>
+        </div>
     )
 }
 export default Loader;
